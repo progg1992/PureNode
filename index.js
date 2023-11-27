7de0228641ee28c974c2c25d5ef667ad9615a772
progg1992 <pierce.rogg@mymail.nwtc.edu>
2023-11-26 20:43:18 -0600

    Added url parsing

--- index.js
+++ index.js
@@ -5,10 +5,21 @@
 
 //Dependencies
 const http = require('http');
-
+const url = require('url');
 // The server should respond to all requests with a string
 const server = http.createServer(function (req, res) {
+	// Get the URL and parse it
+	let parsedUrl = url.parse(req.url, true);
+
+	// Get the path
+	let urlPath = parsedUrl.pathname;
+	let trimmedPath = urlPath.replace(/^\/+|\/+$/g, '');
+
+	// Send the response
 	res.end('Hello World\n');
+
+	// Log the request path
+	console.log('Request is received on path: ' + trimmedPath);
 });
 
 // Start the server, and have it listen on port 3000
